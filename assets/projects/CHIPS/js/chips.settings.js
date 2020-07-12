65b91e0c263c029ec89ce2114c813c745f273f40
Jacob Brogan <6467770+dolphinspired@users.noreply.github.com>
2020-07-12 15:21:26 -0400

    Remove old site contents

--- assets/projects/CHIPS/js/chips.settings.js
+++
@@ -1,7 +0,0 @@
-/* The following variables MUST be included with any application. */
-
-// To be used in the future for user settings
-
-chips.settings = {
-
-};
--- blog/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Dolphinspired's Weblog
-layout: home
-list_title: " "
----
-Reflection and introspection on games, software, and dogs.
